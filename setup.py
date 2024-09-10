52432c3b5bb7143806bacbe31cd73476a691eb1a
Jacob Henry Freeman <jacob@saronic.com>
2024-09-10 16:32:30 +0000

    wip

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
-"""
-Copyright (c) Meta Platforms, Inc. and affiliates.
-All rights reserved.
-
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree.
-
-"""
-
 from setuptools import find_packages, setup
+import os
 
-if __name__ == "__main__":
-    setup(
-        name="distributed_shampoo",
-        version="0.1.0",
-        description="PyTorch implementation of Distributed Shampoo",
-        license="BSD 3-clause",
-        packages=find_packages(),
-        package_data={'': ['*']},  # Include all files in all packages
-        install_requires=[],
-        python_requires=">=3.8",
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Science/Research",
-            "Intended Audience :: Developers",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        ],
-    )
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('distributed_shampoo')
+
+setup(
+    name="distributed_shampoo",
+    version="0.1.0",
+    description="PyTorch implementation of Distributed Shampoo",
+    license="BSD 3-clause",
+    packages=find_packages(),
+    package_data={'distributed_shampoo': extra_files},
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
