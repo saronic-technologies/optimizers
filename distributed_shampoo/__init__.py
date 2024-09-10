3330c09854fe56bcd70832d4d7116043d540c8d0
Jacob Henry Freeman <jacob@saronic.com>
2024-09-10 16:44:25 +0000

    wip

---
+++ distributed_shampoo/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# Get the directory containing this file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add the current directory to the Python path
+sys.path.insert(0, current_dir)
